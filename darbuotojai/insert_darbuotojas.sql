2ed7b567da8a7f449acdf7f374b1d3fcbef57ed2
airidalabas <airidajuraitiene@gmail.com>
2023-01-17 08:18:13 +0200

    insert and dalete darbuotojai

---
+++ darbuotojai/insert_darbuotojas.sql
@@ -0,0 +1,4 @@
+-- SQLite
+INSERT INTO darbuotojai(vardas, pavarde, atlyginimas)
+VALUES ("Mantas", "Dan", 6000);
+SELECT * from darbuotojai; --viska sukeliame is darbuotoju failo
---
+++ darbuotojai/trinti.sql
@@ -0,0 +1,3 @@
+-- SQLite
+DELETE FROM darbuotojai WHERE id >= 2;
+SELECT * FROM darbuotojai;
