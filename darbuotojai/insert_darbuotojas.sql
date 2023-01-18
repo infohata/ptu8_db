9bc3b666b06858ba25878fcd94584de0fdcef6da
Romas1975 <romaslinkevicius@gmail.com>
2023-01-18 18:14:14 +0200

    insert darbuotojas

--- darbuotojai/insert_darbuotojas.sql
+++ darbuotojai/insert_darbuotojas.sql
@@ -1,4 +1,4 @@
 -- SQLite
-INSERT INTO darbuotojai (vardas, pavarde, atlyginimas)
-VALUES ("Egle", "Smaguolyte", 1500);
+INSERT INTO darbuotojai (vardas, pavarde, gimimo_data, pareigos, skyrius_pavadinimas)
+VALUES ("Aukse", "Grybiene", 1970-01-06, "Buhalteris", "Finansai");
 SELECT * FROM darbuotojai;
