59fdbcb2e81e9de8463b69578b08efb5c4669870
Romas1975 <romaslinkevicius@gmail.com>
2023-01-19 11:06:32 +0200

    delete and select

--- darbuotojai/delete_darbuotojai.sql
+++ darbuotojai/delete_darbuotojai.sql
@@ -1,3 +1,3 @@
-DELETE FROM darbuotojai WHERE GIMIMO_DATA = 1963;
+DELETE FROM darbuotojai WHERE GIMIMO_DATA = "1970-01-06";
 SELECT * FROM darbuotojai;
  
--- darbuotojai/select_darbuotojai.sql
+++ darbuotojai/select_darbuotojai.sql
@@ -1,2 +1,3 @@
 -- SQLite
 SELECT from DARBUOTOJAI;
+
